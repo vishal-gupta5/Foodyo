c99f54d80a1f7ad83dac8d287d2857b7b5b65915
Vishal Gupta <guptaaryan1103@gmail.com>
2026-03-05 20:00:26 +0530

    Middleware is update

--- Backend/src/middlewares/auth.js
+++ Backend/src/middlewares/auth.js
@@ -1,17 +1,20 @@
-
-
 const authFoodPartnerMiddleware = async (req, res, next) => {
-    try {
-        const token = res.cookie.token;
-        
-        if (!token) {
-            return res.status(400).json({message: "Token is Invalid!", success: false});
-        }
+  try {
+    const token = res.cookie.token;
 
-        const decode = jwt.verify(token, process.env.JWT_SECRET_KEY);
-        
-    } catch (err) {
-        console.log(`Error: ${err}`);
-        res.status(500).json({message: "Something went wrong!", success: false});
+    if (!token) {
+      return res
+        .status(400)
+        .json({ message: "Token is Invalid!", success: false });
     }
-}
+
+    const decode = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    if (!decode) {
+      return res.status(400).json({ message: "Invalid token", success: false });
+    }
+  } catch (err) {
+    console.log(`Error: ${err}`);
+    res.status(500).json({ message: "Something went wrong!", success: false });
+  }
+};
